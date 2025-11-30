9cc3a255efef1fc9a4af9f337c1c7b4ac7b967c4
Akinn Rosa <akinncar@hotmail.com>
2025-11-30 12:50:01 -0300

    feat: init description

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,13 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Hello 👋
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm Akinn, a software engineer, music producer/dj, investor, youtuber, coffe lover, and a bunch of other things.`}
+      </p>
+      <p className="mb-4">
+        {`Let me tell you about myself`}
       </p>
       <div className="my-8">
         <BlogPosts />
